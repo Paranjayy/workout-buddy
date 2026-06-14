edf92861cbdbb3d59660a9d4b280d5d8b13ec070
paranjay <kparanjay245@gmail.com>
2026-06-14 18:21:55 +0530

    fix: restore tab and form input styles, fix gooey button clipping, and correct header text line-height

--- src/components/GooeyButton.tsx
+++ src/components/GooeyButton.tsx
@@ -10,7 +10,7 @@ export const GooeyButton: React.FC<GooeyButtonProps> = ({ children, variant = 'p
     <>
       <svg style={{ visibility: 'hidden', position: 'absolute' }} width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
         <defs>
-          <filter id="goo">
+          <filter id="goo" x="-30%" y="-30%" width="160%" height="160%">
             <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
             <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
             <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
@@ -34,7 +34,7 @@ export const GooeyButton: React.FC<GooeyButtonProps> = ({ children, variant = 'p
       <style>{`
         .gooey-btn {
           position: relative;
-          padding: var(--sp-2) var(--sp-6);
+          padding: var(--sp-3) var(--sp-6);
           font-size: var(--fs-sm);
           font-weight: 700;
           color: #fff;
